69e6ff830fcae6ddef5c070610321c999011bc49
taotao54321 <taotao54321@users.noreply.github.com>
2021-11-20 13:12:19 +0000

    deploy: 5fe3143349a125a0d3163f9c5661e51569f8563a

---
+++ pkg/package_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function start(): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h129c5aa259db4552(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8a6a3c4ba55cc886(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h695656335550f1e8(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h5d5c1529a18658df(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke3_mut__h02e7bca9c524570d(a: number, b: number, c: number, d: number, e: number): void;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_start(): void;
